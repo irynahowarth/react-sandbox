46069476469935e8a5c3d6e2d4d5957828c9a188
Iryna Howarth <irynahowarth@gmail.com>
2024-02-15 18:14:54 +0000

    [chore]: commit minor changes before doing new stuff

--- src/dnd-kit/index.jsx
+++ src/dnd-kit/index.jsx
@@ -4,23 +4,6 @@ import Draggable from "./Draggable";
 import Droppable from "./Droppable";
 import Board from "./Board";
 
-// export default function DndKit() {
-//   const [isDropped, setIsDropped] = React.useState(false);
-//   const draggableMarkup = <Draggable>Drag me</Draggable>;
-
-//   return (
-//     <DndContext onDragEnd={handleDragEnd}>
-//       {!isDropped ? draggableMarkup : null}
-//       <Droppable>{isDropped ? draggableMarkup : "Drop here"}</Droppable>
-//     </DndContext>
-//   );
-//   function handleDragEnd(event) {
-//     if (event.over && event.over.id === "droppable") {
-//       setIsDropped(true);
-//     }
-//   }
-// }
-
 export default function DndKit() {
   return <Board />;
 }
